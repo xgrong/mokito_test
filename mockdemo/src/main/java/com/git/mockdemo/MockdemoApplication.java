ecd3acb8ea8db852fc129b751243faf776808f3d
xgrong <459671319@qq.com>
2020-06-23 17:57:35 +0800

    新建

---
+++ mockdemo/src/main/java/com/git/mockdemo/MockdemoApplication.java
@@ -0,0 +1,13 @@
+package com.git.mockdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockdemoApplication.class, args);
+	}
+
+}
---
+++ mockdemo/src/test/java/com/git/mockdemo/MockdemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.git.mockdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+}
